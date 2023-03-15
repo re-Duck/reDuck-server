66428895e1208378e57751615bd9f083f8d338e7
Kimdonghun <zhfptm12@naver.com>
2023-03-16 02:23:50 +0900

    Init : Project init

---
+++ src/main/java/reduck/reduck/ReduckApplication.java
@@ -0,0 +1,13 @@
+package reduck.reduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReduckApplication.class, args);
+	}
+
+}
