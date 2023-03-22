67ab6fef0f44a3d6992722da6b03a1a6e8fb169a
kimdonghun <zhfptm12@naver.com>
2023-03-22 19:58:01 +0900

    Feat : member클래스 인덱스 및 필드 추가

--- src/main/java/reduck/reduck/domain/user/entity/Member.java
+++ src/main/java/reduck/reduck/domain/user/entity/Member.java
@@ -9,17 +9,21 @@ import reduck.reduck.global.entity.BaseEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = @Index(name = "idx_memberId", columnList = "memberId", unique = true))
 public class Member extends BaseEntity {
 
     @Column(length = 20)
     String password;
-    @Column(length = 20)
+    @Column(length = 20, unique = true)
     String memberId;
-
+    @Column(length = 20)
+    String name;
 }
