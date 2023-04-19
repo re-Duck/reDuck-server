1cd59747c8397f169c0e4af9962e4c9f7fdb999a
Kimdonghun <zhfptm12@naver.com>
2023-04-19 14:38:39 +0900

    Refactor : #6

--- src/test/java/reduck/reduck/domain/user/service/UserServiceTest.java
+++ src/test/java/reduck/reduck/domain/user/service/UserServiceTest.java
@@ -1,17 +1,47 @@
 package reduck.reduck.domain.user.service;
 
+import com.google.gson.Gson;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.Part;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockPart;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.context.WebApplicationContext;
 import reduck.reduck.domain.auth.repository.AuthRepository;
 import reduck.reduck.domain.auth.service.AuthService;
+import reduck.reduck.domain.user.dto.SignUpDto;
+import reduck.reduck.domain.user.entity.User;
 import reduck.reduck.domain.user.repository.UserRepository;
 import reduck.reduck.global.security.JwtProvider;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import javax.transaction.Transactional;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class UserServiceTest {
     @Autowired
@@ -26,48 +56,93 @@ class UserServiceTest {
     AuthService authService;
     @Autowired
     private WebApplicationContext webApplicationContext;
+    @Autowired
+    private MockMvc mockMvc;
 
-//    @Test
-//    @Transactional
-//    void signUp() throws Exception {
-        //정상작동.
-//        SignUpDto signUpDto = SignUpDto.builder()
-//                .userId("test2")
-//                .password("1234")
-//                .name("nuhgnod")
-//                .email("1234@naver.com")
-//                .developAnnual("8")
-//                .company(null)
-//                .school(null)
-//                .build();
-//        MockMultipartFile file
-//                = new MockMultipartFile(
-//                "file",
-//                "hello.txt",
-//                MediaType.TEXT_PLAIN_VALUE,
-//                "Hello, World!".getBytes()
-//        );
-//
-////        MockMvc mockMvc
-////                = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-////        mockMvc.perform(multipart("/user/test1").file(file))
-////                .andExpect(status().isOk());
-//        userService.signUp(signUpDto, file);
-//        System.out.println("=====================================================================");
-//        //이미 존재하는 아이디로 회원가입.
-//        SignUpDto signUpDto2 = SignUpDto.builder()
-//                .userId("test2")
-//                .password("1234")
-//                .name("nuhgnod")
-//                .email("1234@naver.com")
-//                .developAnnual("8")
-//                .company(null)
-//                .school(null)
-//                .build();
-//        Assertions.assertThatThrownBy(() -> {
-//            userService.signUp(signUpDto2, file);
-//        }).isInstanceOf(Exception.class);
-//    }
+    @Autowired
+    private Gson gson;
+
+    @Transactional
+    @DisplayName("정상 회원가입")
+    @ParameterizedTest
+    @CsvSource("test1,p39pwt12!, donghun, zhfptm12@naver.com,3,naver,CNU")
+    void 정상회원가입(String userId, String password, String name, String email, String developAnnual, String company, String school) throws Exception {
+        System.out.println("==================================================================");
+        System.out.println("userId = " + userId + ", password = " + password + ", name = " + name + ", email = " + email + ", developAnnual = " + developAnnual + ", company = " + company + ", school = " + school);
+        System.out.println("==================================================================");
+        MockMultipartFile file
+                = new MockMultipartFile(
+                "file",
+                "hello.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "Hello, World!".getBytes()
+        );
+        SignUpDto signUpDto = SignUpDto.builder()
+                .userId(userId)
+                .password(password)
+                .name(name)
+                .email(email)
+                .developAnnual(developAnnual)
+                .company(company)
+                .school(school)
+                .build();
+        User user = userService.signUp(signUpDto, file);
+        String format1 = user.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));//spring boot ms 9자리, mysql6자리
+        String format2 = user.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));//spring boot ms 9자리, mysql6자리
+        user.setCreatedAt(LocalDateTime.parse(format1, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")));
+        user.setUpdatedAt(LocalDateTime.parse(format2, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")));
+        User byUserId = userService.findByUserId(user.getUserId());
+        Assertions.assertThat(user).isEqualTo(byUserId);
+    }
+
+    @Transactional
+    @DisplayName("회원가입 유효성 검사")
+    @ParameterizedTest
+    @MethodSource("provideUserObject")
+    void 회원가입유효성검사(String userId, String password, String name, String email, String developAnnual, String company, String school, MockMultipartFile file,Exception exception) throws Exception {
+        System.out.println("==================================================================");
+        System.out.println("userId = " + userId + ", password = " + password + ", name = " + name + ", email = " + email + ", developAnnual = " + developAnnual + ", company = " + company + ", school = " + school);
+        System.out.println("==================================================================");
+        SignUpDto signUpDto = SignUpDto.builder()
+                .userId(userId)
+                .password(password)
+                .name(name)
+                .email(email)
+                .developAnnual(developAnnual)
+                .company(company)
+                .school(school)
+                .build();
+        String path = "/user/" + userId;
+        MockMultipartFile jsonPart = new MockMultipartFile("signUpDto", "signUpDto", "application/json", gson.toJson(signUpDto).getBytes(StandardCharsets.UTF_8));
+
+        ResultActions resultActions = mockMvc.perform(multipart(path)
+                        .file(file)
+                        .file(jsonPart)
+                )
+                .andDo(System.out::println).andExpect(status().isBadRequest()).andExpect(
+                        ex -> org.junit.jupiter.api.Assertions.assertTrue(ex.getResolvedException()
+                                .getClass()
+                                .isAssignableFrom(exception.getClass())));
+
+    }
+
+    private static Stream<Arguments> provideUserObject() {
+        MockMultipartFile file
+                = new MockMultipartFile(
+                "multipartFile",
+                "hello.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "Hello, World!".getBytes()
+        );
+
+        return Stream.of(
+                Arguments.of("test1", "p39pwt12!", "donghun", "zhfptm12@naver.com", "3", "naver", "CNU", file, DataIntegrityViolationException.class), //아이디 중복
+                Arguments.of("test12", "p39pwt12~!", "donghun", "zhfptm12@naver.com", "3", "naver", "CNU", file, MethodArgumentNotValidException.class)
+//                Arguments.of("test3","p39pwt12!","donghun", "zhfptm12@naver.com", "3","naver" ,"CNU"),
+//                Arguments.of("test4","p39pwt12!","donghun", "zhfptm12@naver.com", "3","naver" ,"CNU")
+
+        );
+    }
 //
 //    @Test @Transactional
 //    void signIn() throws Exception {
