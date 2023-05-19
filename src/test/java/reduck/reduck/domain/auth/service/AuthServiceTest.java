f64e9eb7426c35d6e4474cea42da2b8d3a1b7c77
Kimdonghun <zhfptm12@naver.com>
2023-05-19 14:14:08 +0900

    Test : login 테스트 수정

--- src/test/java/reduck/reduck/domain/auth/service/AuthServiceTest.java
+++ src/test/java/reduck/reduck/domain/auth/service/AuthServiceTest.java
@@ -14,8 +14,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import reduck.reduck.domain.auth.dto.SignInDto;
 import reduck.reduck.domain.auth.repository.AuthRepository;
 import reduck.reduck.domain.user.repository.UserRepository;
+
 import javax.transaction.Transactional;
 import java.nio.charset.StandardCharsets;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,7 +33,6 @@ class AuthServiceTest {
     AuthRepository authRepository;
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private Gson gson;
 
@@ -39,16 +40,16 @@ class AuthServiceTest {
     @DisplayName("정상 회원가입")
     @ParameterizedTest
     @CsvSource("test2, p39pwt12!, donghun, zhfptm12@naver.com,2022,naver,CNU")
-    MockHttpServletResponse signIn() throws Exception {
+    void signIn() throws Exception {
+
         SignInDto dto = new SignInDto();
         dto.setPassword("p39pwt12!");
         dto.setUserId("test1");
-        byte[] bytes = gson.toJson(dto).getBytes(StandardCharsets.UTF_8);
-        MvcResult login = mockMvc.perform(post("/login").content(bytes).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andReturn();
-        System.out.println(login);
-        MockHttpServletResponse response = login.getResponse();
-        System.out.println("response = " + response);
-        return response;
+        String s = gson.toJson(dto);
+        mockMvc.perform(post("/login")
+                        .content(s)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+
     }
 }
