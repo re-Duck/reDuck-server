528b41fe79272c4fc853d3b96c95409ecf9b349c
kimdonghun <zhfptm12@naver.com>
2023-05-24 20:05:10 +0900

    Refactor : 이메일 인증 api url수정 및 파라미터명 수정

--- src/main/java/reduck/reduck/domain/auth/controller/EmailController.java
+++ src/main/java/reduck/reduck/domain/auth/controller/EmailController.java
@@ -22,16 +22,23 @@ public class EmailController {
 
     private final EmailService emailService;
 
-    @PostMapping("/email/number") // 인증번호 요청
-    public ResponseEntity<Void> sendUserEmailAuthenticationNumber(@RequestBody @Valid EmailRequestDto emailRequestDto) throws MessagingException, UnsupportedEncodingException {
-        emailService.sendEmail(emailRequestDto);
+    @PostMapping("/email/user/number") //회원가입
+    public ResponseEntity<Void> sendUserEmailAuthenticationNumber(@RequestBody @Valid EmailRequestDto userEmailRequestDto) throws MessagingException, UnsupportedEncodingException {
+        emailService.sendEmail(userEmailRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
-    @PostMapping("/email") // 인증 요청
-    public ResponseEntity<EmailAuthenticateResponseDto> authenticateUserEmail(@RequestBody @Valid EmailAuthenticateRequestDto emailRequestDto) {
-        return new ResponseEntity(emailService.authenticateEmail(emailRequestDto), HttpStatus.CREATED);
+    @PostMapping("/email/profile/number") // 마이페이지
+    public ResponseEntity<Void> sendEmailAuthenticationNumber(@RequestBody @Valid EmailRequestDto profileEmailRequestDto) throws MessagingException, UnsupportedEncodingException {
+        emailService.sendEmail(profileEmailRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
+    @PostMapping("/email/user") //회원가입
+    public ResponseEntity<EmailAuthenticateResponseDto> authenticateUserEmail(@RequestBody @Valid EmailAuthenticateRequestDto userEmailAuthRequestDto) {
+        return new ResponseEntity(emailService.authenticateEmail(userEmailAuthRequestDto), HttpStatus.CREATED);
+    }
+    @PostMapping("/email/profile") //마이 페이지
+    public ResponseEntity<EmailAuthenticateResponseDto> authenticateEmail(@RequestBody @Valid EmailAuthenticateRequestDto profileEmailAuthRequestDto) {
+        return new ResponseEntity(emailService.authenticateEmail(profileEmailAuthRequestDto), HttpStatus.CREATED);
+    }
 
 }
