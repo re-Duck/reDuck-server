ec4c91d33c54b3ab182c52f8738ea81bf5a005f3
Kimdonghun <zhfptm12@naver.com>
2023-08-24 01:51:32 +0900

    Feat : #5 Session table과 servie 생성

--- src/main/java/reduck/reduck/domain/chat/entity/Session.java
+++ src/main/java/reduck/reduck/domain/chat/entity/Session.java
@@ -1,9 +1,23 @@
 package reduck.reduck.domain.chat.entity;
 
+import lombok.*;
 import reduck.reduck.global.entity.BaseEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
+@Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Session extends BaseEntity {
+
+    private String session_id;
+
+    private String user_id;
+
+    private String room_id;
+
 }
---
+++ src/main/java/reduck/reduck/domain/chat/service/StompInterceptorService.java
@@ -0,0 +1,10 @@
+package reduck.reduck.domain.chat.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StompInterceptorService {
+    public void disscribe() {
+        // 소켓 끊김 시, DB의 세션 관리.
+    }
+}
