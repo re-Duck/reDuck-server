08a0f925435bd694f3c053f4d1981262ffe42a3b
kimdonghun <zhfptm12@naver.com>
2023-08-30 03:05:07 +0900

    Fix : #51

--- src/main/java/reduck/reduck/domain/chat/service/SimpleChatService.java
+++ src/main/java/reduck/reduck/domain/chat/service/SimpleChatService.java
@@ -109,6 +109,7 @@ public class SimpleChatService extends ChatService {
         List<ChatMessagesResDto> chatMessagesResDtos = ChatMessagesResDtoMapper.from(chatMessages);
 
         return ChatRoomResDto.builder()
+                .roomId(roomId)
                 .chatMessages(chatMessagesResDtos)
                 .build();
     }
@@ -118,7 +119,6 @@ public class SimpleChatService extends ChatService {
     @Transactional
     public String createRoom(ChatRoomReqDto chatRoomReqDto) {
         String userId = AuthenticationToken.getUserId();
-        String roomId = chatRoomReqDto.getRoomId();
         // 먼저 생성된 채팅 방이 있으면 그 채팅방 리턴.
         List<String> participantIds = mergeParticipantIds(userId, chatRoomReqDto.getOtherIds());
         return createRoomIfAbsent(chatRoomReqDto, participantIds);
@@ -183,8 +183,35 @@ public class SimpleChatService extends ChatService {
     }
 
 
-//    @Override
+    //    @Override
     private void postSend(Message<?> message, ChatMessageReqDto dto) {
+        StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+        String sessionId = headerAccessor.getSessionId();
+
+        // 현재 세션에 해당되는 roomId
+        Session mySession = sessionRepository.findBySessionId(sessionId).orElseThrow(() -> new ChatException(ChatErrorCode.SESSION_NOT_EXIST));
+
+        // roomId에 대한 참여 user Id목록을 가져오고,
+        ChatRoom room = mySession.getRoom();
+        String userId = mySession.getUserId();
+        User user = userRepository.findByUserId(userId).get();
+
+        // Send된 message를 json으로 변환 후 messageId 가져옴.
+        String messageId = resolveMessage(message);
+        ChatMessage chatMessage = chatMessageRepository.findByMessageId(messageId).get(); // 현재 전송 된 메시지.
+
+        List<User> others = getOtherUsersInChatRoom(room, user);// 채팅 방 별, 나를 제외한 다른 사용자들.
+        for (User other : others) { // 다른 참여자들의 session 상태에 따라 last_chat_message필드 update.
+            Optional<Session> optSession = sessionRepository.findByUserIdAndRoom(other.getUserId(), room);
+            if (optSession.isPresent()) {
+                Session otherSession = optSession.get();
+                updateLastChatMessage(otherSession, other, room, chatMessage);
+
+            }
+        }
+    }
+
+    private String resolveMessage(Message<?> message) {
         // Session 테이블에 각 room별로 user의 session Id는 고정.
         System.out.println(message.toString());
         byte[] jsonData = (byte[]) message.getPayload(); // 바이트 배열
@@ -196,59 +223,22 @@ public class SimpleChatService extends ChatService {
             throw new RuntimeException(e);
         }
         System.out.println("resultMap = " + resultMap);
-        String messageId = (String) resultMap.get("messageId");
-
-        ChatMessage chatMessage = chatMessageRepository.findByMessageId(messageId).get(); // 현재 전송 된 메시지.
-
-        System.out.println("!@#$!@$!@!$!$!#$!$!@$!@$!#@$#!$!#@!#$!#$");
-        StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        String sessionId = headerAccessor.getSessionId();
-        System.out.println("### sessionId = " + sessionId);
-
-        // 현재 세션에 해당되는 roomId
-        Session mySession = sessionRepository.findBySessionId(sessionId).orElseThrow(() -> new ChatException(ChatErrorCode.SESSION_NOT_EXIST));
-
-        // roomId에 대한 참여 user Id목록을 가져오고,
-        ChatRoom room = mySession.getRoom();
-        String userId = mySession.getUserId();
-        User user = userRepository.findByUserId(userId).get();
-        List<User> others = getOtherUsersInChatRoom(room, user);// 채팅 방 별, 나를 제외한 다른 사용자들.
-        for (User other : others) {
-            Session otherSession = sessionRepository.findByUserIdAndRoom(other.getUserId(), room).orElseThrow(() -> new ChatException(ChatErrorCode.SESSION_NOT_EXIST));
-            SessionStatus otherSessionStatus = otherSession.getStatus();
-            if (otherSessionStatus == SessionStatus.ON) {
-                // 접속 중 : 상대방의 마지막 읽은 메시지 를 현재 message로 업데이트
-
-                ChatRoomUsers chatRoomInfoOfOther = chatRoomUsersRepository.findByUserAndRoom(other, room);
-                chatRoomInfoOfOther.updateLastChatMessage(chatMessage);
-                chatRoomUsersRepository.save(chatRoomInfoOfOther);
-            }
-        }
-        // 상대에 해당되는 session상태로 접속 여부 파악.
-//        others.stream().map(other -> {
-//            Session otherSession = sessionRepository.findByUserIdAndRoom(other.getUserId(), room).orElseThrow(() -> new ChatException(ChatErrorCode.SESSION_NOT_EXIST));
-//            SessionStatus otherSessionStatus = otherSession.getStatus();
-//            if (otherSessionStatus == SessionStatus.ON) {
-//                // 접속 중 : 상대방의 마지막 읽은 메시지 를 현재 message로 업데이트
-//                ChatMessage chatMessage = chatMessageRepository.findByMessageId(messageId).get();
-//
-//                ChatRoomUsers chatRoomInfoOfOther = chatRoomUsersRepository.findByUserAndRoom(other, room);
-//                chatRoomInfoOfOther.updateLastChatMessage(chatMessage);
-//                chatRoomUsersRepository.save(chatRoomInfoOfOther);
-//            }
-//            return null;
-//        });
-
-        // id가 없다면
-        // 채팅방에 없는 상태 : 그대로 둠.
+        return (String) resultMap.get("messageId");
 
     }
 
+    private void updateLastChatMessage(Session otherSession, User other, ChatRoom room, ChatMessage chatMessage) {
+        SessionStatus otherSessionStatus = otherSession.getStatus();
+        if (otherSessionStatus == SessionStatus.ON) {
+            // 접속 중 : 상대방의 마지막 읽은 메시지 를 현재 message로 업데이트
+            ChatRoomUsers chatRoomInfoOfOther = chatRoomUsersRepository.findByUserAndRoom(other, room);
+            chatRoomInfoOfOther.updateLastChatMessage(chatMessage);
+            chatRoomUsersRepository.save(chatRoomInfoOfOther);
+        }
+    }
     /**
      * 채팅방 입장 시 등록됨.
      */
-
-
     private String createAlias(List<String> participantIds) {
         Objects.requireNonNull(participantIds);
         return participantIds.stream()
