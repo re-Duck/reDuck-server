31d022f2c398e26e194cf9c5bd8c1db11208c122
kimdonghun <zhfptm12@naver.com>
2024-01-12 20:55:46 +0900

    fix : aws data transfer 과금 문제로 인한 일시적 제한

--- src/main/java/reduck/reduck/domain/image/ImageController.java
+++ src/main/java/reduck/reduck/domain/image/ImageController.java
@@ -34,33 +34,14 @@ public class ImageController {
     }
     @GetMapping("/home/ubuntu/reduck/storage/{type}/{userId}/{name}")
     public ResponseEntity<Resource> getProdImage(@PathVariable String type, @PathVariable String userId,@PathVariable String name) throws IOException {
-        System.out.println(type);
-        System.out.println(userId);
-        System.out.println(name);
-//        return ResponseEntity.ok().build();
-        Path path = Paths.get("/home/ubuntu/reduck/storage" + "/" +type+ "/" +userId+ "/" +name);
-        String contentType = Files.probeContentType(path);
-
-        HttpHeaders headers = new HttpHeaders();
-        Resource resource = new InputStreamResource(Files.newInputStream(path));
-
-        headers.add(HttpHeaders.CONTENT_TYPE, contentType);
-        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
-    }
-    @GetMapping("/home/nuhgnod/develup/{storage}/{type}/mayack/{userId}/{name}")
-    public ResponseEntity<Resource> getMayackImage(@PathVariable String storage,@PathVariable String type, @PathVariable String userId,@PathVariable String name) throws IOException {
-        System.out.println(storage);
-        System.out.println(type);
-        System.out.println(userId);
-        System.out.println(name);
-//        return ResponseEntity.ok().build();
-        Path path = Paths.get("/home/nuhgnod/develup/" + storage + "/" +type+ "/mayack/" +userId+ "/" +name);
-        String contentType = Files.probeContentType(path);
-
-        HttpHeaders headers = new HttpHeaders();
-        Resource resource = new InputStreamResource(Files.newInputStream(path));
-
-        headers.add(HttpHeaders.CONTENT_TYPE, contentType);
-        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+//        Path path = Paths.get("/home/ubuntu/reduck/storage" + "/" +type+ "/" +userId+ "/" +name);
+//        String contentType = Files.probeContentType(path);
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        Resource resource = new InputStreamResource(Files.newInputStream(path));
+//
+//        headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+//        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
